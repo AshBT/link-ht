2da52b1cc309abdab575fd21db27a9081d0ad5fa
Matt Faulkner <mnfaulk@gmail.com>
2015-05-06 12:07:25 -0700

    Adds images to carousel in entity summaries

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -20,16 +20,28 @@ angular.module('memexLinkerApp')
             var ads = lodash.map(res, function(element){ 
               return element.ad._data.data;
             });
+            //console.log('Ads');
+            //console.log(ads);
             var postTimes = lodash.map(ads, function(ad){
                 // TODO: 
                 return new Date(ad.posttime);
               });
-            $scope.entities.push({
+            var imageUrls = lodash.flatten(
+              lodash.map(ads, function(ad) {
+                return ad.image_locations;
+              }),
+              true
+            );
+
+            var entitySummary = {
               id: entity.id,
               phone: entity.phone,
               nPosts: ads.length,
-              postTimes : postTimes
-            });
+              postTimes : postTimes,
+              imageUrls: imageUrls
+            };
+            console.log(entitySummary);
+            $scope.entities.push(entitySummary);
           });
         });
 
