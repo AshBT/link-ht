a98d4833a792ede830f7ea33edccb9fbeae065ca
Matt Faulkner <mnfaulk@gmail.com>
2015-05-20 12:11:46 -0700

    preparing for deployment with Docker

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -7,6 +7,7 @@ function requiredProcessEnv(name) {
   if(!process.env[name]) {
     throw new Error('You must set the ' + name + ' environment variable');
   }
+  console.log('Using: ' + process.env[name]);
   return process.env[name];
 }
 
