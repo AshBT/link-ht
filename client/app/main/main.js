c37d10e8c51792bb62f1579b914424a2fa1e1a92
Mo T <mo@qadium.com>
2015-06-01 23:36:01 -0700

    WIP to continue tomorrow on filters

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -9,3 +9,27 @@ angular.module('memexLinkerApp')
         controller: 'MainCtrl'
       });
   });
+
+angular.module('memexLinkerApp').filter('gt', function () {
+    return function ( items, value ) {
+        var filteredItems = []
+        angular.forEach(items, function ( item ) {
+            if ( item > value ) {
+                filteredItems.push(item);
+            }
+        });
+        return filteredItems;
+    }
+})
+
+angular.module('memexLinkerApp').filter('lt', function () {
+    return function ( items, value ) {
+        var filteredItems = []
+        angular.forEach(items, function ( item ) {
+            if ( item < value ) {
+                filteredItems.push(item);
+            }
+        });
+        return filteredItems;
+    }
+})
