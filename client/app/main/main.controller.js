b545534d2d4e966d1b85bde3ade6ae60d121b01c
Matt Faulkner <mnfaulk@gmail.com>
2015-06-05 15:47:48 -0700

    Does not show labels for empty entity fields, and minor cleanup

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,45 +4,40 @@ angular.module('memexLinkerApp')
 .controller('MainCtrl', function ($scope, $http, $q, socket, lodash) {
 
   var _ = lodash;
-  var source_map = new Array();
 
-  source_map[1] = 'Backpage';
-  source_map[2] = 'Craigslist';
-  source_map[3] = 'Classivox';
-  source_map[4] = 'MyProviderGuide';
-  source_map[5] = 'NaughtyReviews';
-  source_map[6] = 'RedBook';
-  source_map[7] = 'CityVibe';
-  source_map[8] = 'MassageTroll';
-  source_map[9] = 'RedBookForum';
-  source_map[10] = 'CityXGuide';
-  source_map[11] = 'CityXGuideForum';
-  source_map[12] = 'RubAds';
-  source_map[13] = 'Anunico';
-  source_map[14] = 'SipSap';
-  source_map[15] = 'EscortsInCollege';
-  source_map[16] = 'EscortPhoneList';
-  source_map[17] = 'EroticMugshots';
-  source_map[18] = 'EscortsAdsXXX';
-  source_map[19] = 'EscortsinCA';
-  source_map[20] = 'EscortsintheUS';
-  source_map[21] = 'LiveEscortReviws';
-  source_map[22] = 'MyProviderGuideForum';
-  source_map[23] = 'USASexGuide';
-  source_map[24] = 'EroticReview';
-  source_map[25] = 'AdultSearch';
-  source_map[26] = 'HappyMassage';
-  source_map[27] = 'UtopiaGuide';
-  source_map[28] = 'MissingKids';
-
-
-
-
-
+  var source_map = {
+    1 : 'Backpage',
+    2 : 'Craigslist',
+    3 : 'Classivox',
+    4 : 'MyProviderGuide',
+    5 : 'NaughtyReviews',
+    6 : 'RedBook',
+    7 : 'CityVibe',
+    8 :  'MassageTroll',
+    9 : 'RedBookForum',
+    10 : 'CityXGuide',
+    11 : 'CityXGuideForum',
+    12 : 'RubAds',
+    13 : 'Anunico',
+    14 : 'SipSap',
+    15 : 'EscortsInCollege',
+    16 : 'EscortPhoneList',
+    17 : 'EroticMugshots',
+    18 :  'EscortsAdsXXX',
+    19 : 'EscortsinCA',
+    20 : 'EscortsintheUS',
+    21 : 'LiveEscortReviws',
+    22 : 'MyProviderGuideForum',
+    23 : 'USASexGuide',
+    24 : 'EroticReview',
+    25 : 'AdultSearch',
+    26 : 'HappyMassage',
+    27: 'UtopiaGuide',
+    28 : 'MissingKids'
+  };
 
   $scope.logo = 'http://icons.iconarchive.com/icons/icons8/ios7/256/Very-Basic-Paper-Clip-icon.png';
   $scope.blur = true;
-  //var input = [{key:'1', value:'Backpage'},{key:'key2', value:'value2'}];
   $scope.entities = [];
 
     /* 
@@ -60,8 +55,6 @@ angular.module('memexLinkerApp')
       });
     }
 
-    
-
     /*
     * entity: 
     */ 
@@ -78,9 +71,6 @@ angular.module('memexLinkerApp')
           return ad;
         });
 
-
-
-
         var postTimes = _.map(ads, function(ad){
           return new Date(ad.properties.posttime);
         });
@@ -92,13 +82,13 @@ angular.module('memexLinkerApp')
         var maxAges = _.max(age);
         var rate60 = uniqueFlatAndDefined(collectAdProperty(ads, 'rate60'));
         var priceRange = 'Missing' ;
-        if (rate60.length === 1 & rate60[0] != null) {
-          var priceRange = rate60[0] ;
+        if (rate60.length === 1 && rate60[0] != null) {
+          priceRange = rate60[0] ;
         }
         else if (rate60.length > 1) {
-        var priceRange = _.min(rate60) + ' to ' +  _.max(rate60);
+          priceRange = _.min(rate60) + ' to ' +  _.max(rate60);
         }
-        var website=[]
+        var website=[];
         var sourcesid = uniqueFlatAndDefined(collectAdProperty(ads, 'sources_id'));
         for (var i = 0; i < sourcesid.length; i++) { 
           website=website.concat(source_map[sourcesid[i]]);
@@ -134,14 +124,17 @@ angular.module('memexLinkerApp')
 
         var nFaces = face.length;
 
+        // TODO: refactor server to provide all suggested ads, with reason(s) why each was suggested.
         $http.get('api/entities/' + entity.id + '/byimage').success(function(res){
-          var nSuggested = res.length;
+          var nSuggestedByImage = res.length;
 
           var entitySummary = {
             id: entity.id,
             phone: entity.phone,
             nPosts: ads.length,
-            nSuggested: nSuggested,
+            nSuggestedByImage: nSuggestedByImage,
+            nSuggestedByText: 0,
+            nSuggestedByPhone: 0,
             postTimes : postTimes,
             lastPostTime: lastPostTime,
             firstPostTime: firstPostTime,
@@ -165,25 +158,6 @@ angular.module('memexLinkerApp')
 
 return deferred.promise;
 }
-
-
-// function sumAcrossEntities(entities, propertyName) {
-//       return _.sum(entities, function(entity) {
-//         return entities[propertyName];
-//       });
-//     }
-
-
-//$scope.countAds = sumAcrossEntities($scope.entities, nPosts)
-//$scope.countAds = sumAcrossEntities($scope.entities, nPosts)
-//$scope.countAds = sumAcrossEntities($scope.entities, nPosts)
-//$scope.countAds = sumAcrossEntities($scope.entities, nPosts)
-
-
-
-
-
-
 
 $http.get('/api/entities').success(function(res) {
   var entities = _.map(res, function(e){
