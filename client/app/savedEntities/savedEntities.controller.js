49d3c5614ac48231b529a5688e987611a7aaa258
Matt Faulkner <mnfaulk@gmail.com>
2015-06-10 13:59:48 -0700

    removes inaccurate comments

--- client/app/savedEntities/savedEntities.controller.js
+++ client/app/savedEntities/savedEntities.controller.js
@@ -53,8 +53,6 @@ angular.module('memexLinkerApp')
     });
   }
 
-
-  // Temporary hashmap for computing search result aggregate statistics.
   $scope.logo = 'http://icons.iconarchive.com/icons/icons8/ios7/256/Very-Basic-Paper-Clip-icon.png';
   $scope.blur = true;
   $scope.entities = [];
