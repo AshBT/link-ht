529f7ef9a4fb32190942be9d483d7ab8f77fe8b3
Mo T <mo@qadium.com>
2015-06-12 14:17:43 -0700

    Adding faces filter?

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -10,17 +10,21 @@ angular.module('memexLinkerApp')
       });
   });
 
-angular.module('memexLinkerApp').filter('gt', function () {
-    return function ( items, value ) {
-        var filteredItems = []
-        angular.forEach(items, function ( item ) {
-            if ( item > value ) {
+angular.module('memexLinkerApp').filter('nFacesFilter', function () {
+    return function ( items ) {
+        var filteredItems = [];
+        angular.forEach(items, function(item) {
+            console.log("************************");
+            console.log(item);
+            console.log(item.nFaces);
+            console.log("************************");
+
+            if (item.nFaces >=1 ){
                 filteredItems.push(item);
             }
         });
         return filteredItems;
-    }
-})
+}});
 
 angular.module('memexLinkerApp').filter('lt', function () {
     return function ( items, value ) {
