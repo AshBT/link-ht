b34e3efc1d4ec836ee037506fb8b52a2b4849f2e
Eric Chu <eytchu@gmail.com>
2015-08-01 09:14:31 -0700

    moved required env to a utility lib

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -1,15 +1,7 @@
 'use strict';
 
-var path = require('path');
-var _ = require('lodash');
-
-function requiredProcessEnv(name) {
-  if(!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable');
-  }
-  console.log('Using: ' + process.env[name]);
-  return process.env[name];
-}
+var path = require('path'),
+    _ = require('lodash');
 
 // All configurations will extend these options
 // ============================================
