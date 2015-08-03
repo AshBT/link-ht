83ec236938580d474db17e08e5a3e582b137997a
Eric Chu <eytchu@gmail.com>
2015-08-03 14:49:53 -0700

    added neo4j to databases

--- server/databases.js
+++ server/databases.js
@@ -22,12 +22,26 @@ module.exports = (function() {
     });
   }
 
+  var connect_neo4j = function() {
+    var neo4j = require('neo4j');
+    return new neo4j.GraphDatabase({
+        url: config.neo4j.host,
+        auth: {
+          username: config.neo4j.user,
+          password: config.neo4j.password
+        }
+    });
+
+  }
+
   var _es_client = connect_elasticsearch(),
-      _sql_client = connect_mysql();
+      _sql_client = connect_mysql(),
+      _neo4j_client = connect_neo4j();
 
 
   return {
     elasticsearch: _es_client,
-    mysql: _sql_client
+    mysql: _sql_client,
+    neo4j: _neo4j_client
   }
 })();
