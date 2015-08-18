48dfe49b16d63123e34ae1c92c3d91a88a53f78f
mattgroh <matthew.groh@gmail.com>
2015-08-18 00:32:16 -0700

    DONE: Save Entity and Populate Baseball Card in Saved Entities Tab... and now it's time to go to sleep

--- client/app/savedEntities/savedEntities.controller.js
+++ client/app/savedEntities/savedEntities.controller.js
@@ -2,22 +2,154 @@
 
 angular.module('memexLinkerApp')
 .controller('SavedentitiesCtrl', function ($scope, $http, $q, socket, lodash, entityService, linkUtils, Crossfilter) {
-
     var _ = lodash;
     var uniqueFlatAndDefined = linkUtils.uniqueFlatAndDefined;
     var collectAdProperty = linkUtils.collectAdProperty;
     var collectAdProperty2 = linkUtils.collectAdProperty2;
+    var _SEARCH_URL = '/api/v1/search';
+    $scope.entities = []
+    $scope.adIds = []
+    $scope.temp_url=[]
 
-    console.log("/api/annotations/search")
-    $http.get('/api/annotations/search').then(function(response){
-    var x=[]
-    for (var i = 0; i < response.data.length; i++) {
-      x[i] = response.data[i]._source.entityid
+
+  var sources = {
+    1 : 'Backpage',
+    2 : 'Craigslist',
+    3 : 'Classivox',
+    4 : 'MyProviderGuide',
+    5 : 'NaughtyReviews',
+    6 : 'RedBook',
+    7 : 'CityVibe',
+    8 : 'MassageTroll',
+    9 : 'RedBookForum',
+    10 : 'CityXGuide',
+    11 : 'CityXGuideForum',
+    12 : 'RubAds',
+    13 : 'Anunico',
+    14 : 'SipSap',
+    15 : 'EscortsInCollege',
+    16 : 'EscortPhoneList',
+    17 : 'EroticMugshots',
+    18 : 'EscortsAdsXXX',
+    19 : 'EscortsinCA',
+    20 : 'EscortsintheUS',
+    21 : 'LiveEscortReviews',
+    22 : 'MyProviderGuideForum',
+    23 : 'USASexGuide',
+    24 : 'EroticReview',
+    25 : 'AdultSearch',
+    26 : 'HappyMassage',
+    27: 'UtopiaGuide',
+    28 : 'MissingKids'
+  };
+
+function _formatEntity(rawEntity) {
+    
+    var ads = rawEntity._source.base;
+    // Aggregate ad details
+    var postTimes = _.map(ads, function(ad){
+      //console.log(ad);
+      return new Date(ad.posttime);
+    });
+    var lastPostTime = _.max(postTimes);
+    var firstPostTime = _.min(postTimes);
+
+    var ages = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'age')).sort();
+
+    var rates60 = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'rate60')).sort();
+    
+    var websites=[];
+    var sourcesid = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'sources_id'));
+    for (var i = 0; i < sourcesid.length; i++) {
+      websites = websites.concat(sources[sourcesid[i]]);
     }
-    x = (_.uniq(x))
-    console.log(x)
+    websites = _.filter(_.uniq(websites), function(element){
+      return ! _.isUndefined(element);
+    });
+    var phones = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'phone'));
 
-    }, function(response) {
-      console.log("error...")
-      });
-  });
+    var titles = linkUtils.collectAdProperty(ads, 'title');
+    var texts = linkUtils.collectAdProperty(ads, 'text');
+    var snippet1= "";
+    var snippet2= "";
+    var snippet3= "";
+    var all_text = titles + texts;
+    var names = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'name'));
+    var cities = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'city'));
+    for (var i = 0; i < cities.length; i++) {
+          cities[i]=cities[i].substring(0,20);
+        }
+    var youtube = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'youtube'));
+    var instagram = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'instagram'));
+    var twitter = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'twitter'));
+    var socialmedia = twitter + instagram + youtube;
+    var ethnicity = linkUtils.uniqueFlatAndDefined(linkUtils.collectAdProperty(ads, 'ethnicity'));
+    var imageUrls = _.uniq(lodash.flatten(
+      _.map(ads, function(ad) {
+        return ad.image_locations;
+      }),
+          true));
+
+    imageUrls = _.filter(imageUrls, function(element){
+      return ! _.isUndefined(element);
+    });
+    var faceImageUrls = _.uniq(lodash.flatten(
+      _.map(ads, function(ad) {
+        return ad.face_image_url;
+      }),
+      true
+    ));
+    faceImageUrls = _.filter(faceImageUrls, function(element){
+      return ! _.isUndefined(element);
+    });
+  
+    var entity = {
+      id: rawEntity._id,
+      phones: phones,
+      names: names,
+      nPosts: ads.length,
+      firstPostTime: firstPostTime,
+      lastPostTime: lastPostTime,
+      websites: websites,
+      cities: cities,
+      ages: ages,
+      rates60: rates60,
+      imageUrls: imageUrls,
+      nPics: imageUrls.length,
+      faceImageUrls: faceImageUrls,
+      nSuggestedByImage: 0,
+      nSuggestedByPhone: 0,
+      nSuggestedByText: 0,
+      socialmedia: socialmedia,
+      titles: titles,
+      texts: texts,
+      all_text: all_text
+    };
+    return entity;
+  }
+
+    $http.get('/api/annotations/search').then(function(response){
+      var x=[]
+      for (var i = 0; i < response.data.length; i++) {
+        x[i] = response.data[i]._source.entityid
+      }
+      x = _.uniq(x)
+      x = _.filter(x, function(element){
+      return ! _.isUndefined(element);
+    });
+    console.log(x)
+    for (var i = 0; i < x.length; i++) {
+      $scope.adIds.push(x[i])
+      $scope.temp_url.push("http://localhost:9000/entitydetail/" + x[i])
+
+      console.log('Here goes nothing...');
+      $http.post(_SEARCH_URL, {query:x[i]}).then(function(response){
+        $scope.entities = _.map(response.data.entities, function(e) {
+          var entity = _formatEntity(e);
+          console.log(entity)
+          return entity;
+        });
+      })
+    }
+  })
+});
